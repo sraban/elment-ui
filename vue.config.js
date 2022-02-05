c91e42362fc7df5983c33c0dc07a56483f4862ee
sraban <sraban_pvt@yahoo.com>
2022-02-05 16:33:47 +0530

    base url modified

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
+// /FRONTEND/vue/appiness/mang/build/
 module.exports = {
-     publicPath:process.env.NODE_ENV === 'production'? '/FRONTEND/vue/appiness/mang/build/': '/',
+     publicPath:process.env.NODE_ENV === 'production'? '/elment-ui/build/': '/',
      outputDir: path.resolve(__dirname, "build"),      
  }
